8315bc4dab1cb9702fb7b530c54cb4702ea3085f
hollowspecter <tigervb@aol.com>
2014-01-04 20:39:46 +0100

    edited gitignore, removed libs

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	
+	public Vector3 vectorToCamera = new Vector3(-0.2f, -0.15f, 9.0f);
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Camera.main.ViewportToWorldPoint(vectorToCamera);
+	}
+}
---
+++ Assets/Scripts/Level 0/CameraPanDown.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPanDown : MonoBehaviour {
+
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0,6,-10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y > 0)
+		{
+			Vector3 movement = new Vector3(0, transform.position.y - speed * Time.deltaTime, -10);
+			transform.position = movement;
+		}
+	}
+}
